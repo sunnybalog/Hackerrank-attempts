19531b07ee81a5cadf618e87952562335bbe9475
sunnybalog <33362184+sunnybalog@users.noreply.github.com>
2022-04-06 12:48:21 +0100

    Update time-conversion.js

--- time-conversion.js
+++ time-conversion.js
@@ -1,42 +1,28 @@
-// Given a time in
+/* Given a time in
 
-//-hour AM/PM format, convert it to military (24-hour) time.
+-hour AM/PM format, convert it to military (24-hour) time.
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 
-//Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
-//- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+Example
+Return '12:01:00'.
+Return '00:01:00'.
 
-//Example
+Function Description
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+timeConversion has the following parameter(s):
+  string s: a time in 
+   hour format
 
-//Return '12:01:00'.
+returns
+  string: the time in 
+   hour format
+Input Format
+A single string
+that represents a time in -hour clock format (i.e.: or).
 
-   // Return '00:01:00'.
-
-//Function Description
-
-//Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
-
-//timeConversion has the following parameter(s):
-
-  //  string s: a time in 
-
-   // hour format
-
-//Returns
-
-  //  string: the time in 
-
-   // hour format
-
-//Input Format
-
-//A single string
-//that represents a time in -hour clock format (i.e.: or
-
-//).
-
-//Constraints
-
-//    All input times are valid
+Constraints
+    All input times are valid  */
 
 function timeConversion(s) {
     // Write your code here
